3156b70b2fe88d1d133e28219e0bab1bbab7b002
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 15:49:33 +0000

    built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contract'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
