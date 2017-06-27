0f140bcd70c4a817342b1016ad49db5bc8008b5e
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:47:23 +0000

    integrated create and new functions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,25 @@ class PortfoliosController < ApplicationController
   def index
    @portfolio_items = Portfolio.all
   end
+  
+  def new
+ @portfolio_items = Portfolio.new
+
+  end
+  
+  def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+
 end
