add3b5dfe2b67ec600956f432c90e488f288e03e
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 05:45:03 +0000

    added controller concerns for devise params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,10 @@
+ module DeviseWhitelist
+   extend ActiveSupport::Concern
+    included do
+     before_filter :configure_permitted_perameters, if: :devise_controller?
+   end
+  def configure_permitted_perameters
+    devise_parameter_sanatizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanatizer.permit(:account_update, keys: [:name])
+  end
+ end
