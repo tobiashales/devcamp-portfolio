33bb21ea746abbc3d3e0f701d139bee8d8f5508d
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 23:42:55 +0000

    reformatted current user function

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,8 +1,14 @@
- module CurrentUserConcern
+module CurrentUserConcern
  
  def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    super || guest_user
  end
  
- 
+ def guest_user
+  OpenStruct.new(name: "Guest User", 
+           first_name: "Guest",
+           last_name: "User", 
+           email: "guest@example.com"
+           ) 
  end
+end
